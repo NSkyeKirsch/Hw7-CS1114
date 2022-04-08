84cf6cdc2ad0a6cbb6707377abbc673675505988
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-04-07 22:08:44 -0400

    minor style changes

--- hw7_q4.py
+++ hw7_q4.py
@@ -1,3 +1,13 @@
+"""
+Author: Noa Kirschbaum
+Assignment / Part: HW7 - Q4
+Date due: 2022-04-07
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
+
 import super_secret_module
 
 def is_impostor(information, corrupter_function):
---
+++ hw7_q5.py
@@ -0,0 +1,9 @@
+"""
+Author: Noa Kirschbaum
+Assignment / Part: HW7 - Q5
+Date due: 2022-04-07
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
